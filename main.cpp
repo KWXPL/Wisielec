0293c97ce7d6de6fd6eb18a9d755a19c9487fe82
Krzysztof Wisniewski <m1a1osowa@gmail.com>
2023-09-22 18:19:27 +0200

    Dodatkowe funkcje

--- main.cpp
+++ main.cpp
@@ -1,12 +1,51 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <random>
 
 void menu () {
 	std::cout << "Wybierz opcje z menu wybierajac odpowiednia liczbe\n";
 	std::cout << "1. Nowa gra\n";
 	std::cout << "2. Sproboj jeszcze raz\n";
+	switch (menuInput()) {
+	case 1:
+		
+		break;
+	default:
+		break;
+	}
 }
 
-int main () {
+int menuInput () {
+	std::string input;
+	while(true) {
+		std::cin >> input;
+		int number = stoi(input);
+		if (number > 0 || number < 3) {
+			return number;
+		}
+		std::cout << "Nieprawidlowy numer. Wprowadź ponownie numer";
+	}
+}
 
+void newWord() {
+
+
+}
+
+std::vector <std::string> readWordsFromFile() {
+	std::string data;
+	std::vector <std::string> words;
+	std::ifstream input{ "words.txt" };
+	while (input >> data)
+	{
+		words.push_back(data);
+	}
+	return words;
+}
+
+
+int main () {
+	menu();
     return 0;
 }
